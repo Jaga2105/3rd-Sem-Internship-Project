43b5495a2389c2172b8604b217135db547bb6504
Dinesh Rout <rddinesh779@gmail.com>
2022-12-29 14:32:21 +0530

    added upload route

--- client/src/components/patientDashboard/PatientHistory.js
+++ client/src/components/patientDashboard/PatientHistory.js
@@ -1,48 +1,48 @@
 // import Footer from "../landingPage/Footer";
-import patient_profile from "../../assets/img/dashboard/patient2_pbl.png";
-import PatientHistoryCompo from "./PatientHistoryCompo";
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import patient_profile from '../../assets/img/dashboard/patient2_pbl.png';
+import PatientHistoryCompo from './PatientHistoryCompo';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const PatientHistory = (props) => {
   const navigate = useNavigate();
-  const [dob, setDob] = useState("01/01/2006");
+  const [dob, setDob] = useState('01/01/2006');
   const [patient, setPatient] = useState({
     name: {
-      firstName: "",
-      middleName: "",
-      surName: "",
+      firstName: '',
+      middleName: '',
+      surName: '',
     },
-    dob: "",
-    mobile: "",
-    email: "",
-    adharCard: "",
-    bloodGroup: "",
+    dob: '',
+    mobile: '',
+    email: '',
+    adharCard: '',
+    bloodGroup: '',
     address: {
-      building: "",
-      city: "",
-      taluka: "",
-      district: "",
-      state: "",
-      pincode: "",
+      building: '',
+      city: '',
+      taluka: '',
+      district: '',
+      state: '',
+      pincode: '',
     },
-    password: "",
-    diseases: [{ disease: "", yrs: "" }],
+    password: '',
+    diseases: [{ disease: '', yrs: '' }],
     contactPerson: {
       name: {
-        firstName: "",
-        surName: "",
+        firstName: '',
+        surName: '',
       },
-      mobile: "",
-      email: "",
-      relation: "",
+      mobile: '',
+      email: '',
+      relation: '',
       address: {
-        building: "",
-        city: "",
-        taluka: "",
-        district: "",
-        state: "",
-        pincode: "",
+        building: '',
+        city: '',
+        taluka: '',
+        district: '',
+        state: '',
+        pincode: '',
       },
     },
   });
@@ -58,15 +58,15 @@ const PatientHistory = (props) => {
 
   useEffect(() => {
     async function getpatient() {
-      const res = await fetch("/getpatient");
+      const res = await fetch('/getpatient');
       const data = await res.json();
       if (data.AuthError) {
         props.settoastCondition({
-          status: "info",
-          message: "Please Login to proceed!!!",
+          status: 'info',
+          message: 'Please Login to proceed!!!',
         });
         props.setToastShow(true);
-        navigate("/");
+        navigate('/');
       } else {
         setPatient(data.patient);
         if (data.patient.prescriptions) {
@@ -76,36 +76,37 @@ const PatientHistory = (props) => {
       }
     }
     getpatient();
-  }, [dob]);
+  }, [patient.dob, props, navigate]);
+
   return (
-    <div className="col-span-10">
-      <div className=" px-12">
-        <div className="h-screen">
-          <div className="font-poppins   mainf">
-            <Link to="/patient/profile">
-              <div className="flex bg-white rounded shadow  px-4   ml-auto h-14 w-1/5 mr-8 mt-8">
+    <div className='col-span-10'>
+      <div className=' px-12'>
+        <div className='h-screen'>
+          <div className='font-poppins   mainf'>
+            <Link to='/patient/profile'>
+              <div className='flex bg-white rounded shadow  px-4   ml-auto h-14 w-1/5 mr-8 mt-8'>
                 <img
                   src={patient_profile}
-                  className="w-12 p-1 rounded-2xl"
-                  alt="profile"
+                  className='w-12 p-1 rounded-2xl'
+                  alt='profile'
                 ></img>
-                <div className="grid grid-rows-2 ml-4 gap-2  mb-4">
-                  <div className="mt-4 ml-4  font-bold font-poppins">
-                    <h1 className="ml-2">
+                <div className='grid grid-rows-2 ml-4 gap-2  mb-4'>
+                  <div className='mt-4 ml-4  font-bold font-poppins'>
+                    <h1 className='ml-2'>
                       {`${patient.name.firstName} ${patient.name.surName}`}
                     </h1>
                   </div>
                 </div>
               </div>
             </Link>
-            <div className="flex justify-between m-8">
-              <div className="font-bold text-xl ml-4">
+            <div className='flex justify-between m-8'>
+              <div className='font-bold text-xl ml-4'>
                 <h1>Patient Reports</h1>
               </div>
             </div>
-            <div className="bg-white m-4 rounded-lg ">
-              <div className="grid grid-rows-2 p-6 gap-2 shadow">
-                <div className="grid grid-cols-4 font-bold ">
+            <div className='bg-white m-4 rounded-lg '>
+              <div className='grid grid-rows-2 p-6 gap-2 shadow'>
+                <div className='grid grid-cols-4 font-bold '>
                   <div>
                     <h1>Date</h1>
                   </div>
@@ -133,7 +134,7 @@ const PatientHistory = (props) => {
                     );
                   })
                 ) : (
-                  <div className="font-bold mt-3 mx-auto">
+                  <div className='font-bold mt-3 mx-auto'>
                     No Record Found...
                   </div>
                 )}
@@ -143,9 +144,7 @@ const PatientHistory = (props) => {
         </div>
       </div>
 
-      <div className="-mt-20 mb-0">
-        {/* <Footer></Footer> */}
-      </div>
+      <div className='-mt-20 mb-0'>{/* <Footer></Footer> */}</div>
     </div>
   );
 };
